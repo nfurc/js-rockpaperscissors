c73477f89de7d670f750bcd735a8be09a333fa32
Nicholas Furcillo <nfurc@users.noreply.github.com>
2022-09-24 13:11:05 +0200

    Finished!

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,15 +1,12 @@
 // set up
 
 const arr = ["rock", "paper", "scissors"];
+let score = 0;
 
 // functions 
 
-function random(mn, mx) {
-    return Math.random() * (mx - mn) + mn;
-}
-
 function computerChoice(array){
-    return arr[Math.floor(random(1,4))-1];
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function round(computerSelection, playerSelection) {
@@ -18,44 +15,64 @@ function round(computerSelection, playerSelection) {
     let lose = "you lose!";
     let tie = "it's a tie!";
 
+    // messages
+    let youLose = computerSelection + " wins over " + playerSelection;
+    let youWin = playerSelection + " wins over " + computerSelection;
+
     // it's a tie
     if (computerSelection === playerSelection) {
         return tie;
     }
 
-    // losing options
-    if (computerSelection === "rock" && playerSelection === "scissors") {
-        return lose;
-    } else if (computerSelection === "scissors" && playerSelection === "paper") {
-        return lose;
-    } else if (computerSelection === "paper" && playerSelection === "rock") {
-        return lose;
-    }
+    // losing
+    if ((computerSelection === "rock" && playerSelection === "scissors") || 
+        (computerSelection === "scissors" && playerSelection === "paper") ||
+        (computerSelection === "paper" && playerSelection === "rock")) {
+            console.log(youLose);
+            score--;
+            return lose;
+        }
 
-    // winning options
-    if (computerSelection === "rock" && playerSelection === "paper") {
-        return win;
-    } else if (computerSelection === "paper" && playerSelection === "scissors") {
-        return win;
-    } else if (computerSelection === "scissors" && playerSelection === "rock") {
-        return win;
-    }
+    // winning
+    if ((computerSelection === "rock" && playerSelection === "paper") ||
+        (computerSelection === "paper" && playerSelection === "scissors") ||
+        (computerSelection === "scissors" && playerSelection === "rock")) {
+            console.log(youWin);
+            score++;
+            return win;
+        }
 }
 
 // program
-let computerSelection = computerChoice(arr);
 
-console.log("computer: " + computerSelection);
+// game loop 
+while (score < 5)
+    {
+        // intro
+        console.log("Play until you score 5 points! If you lose, you lose a point!");
+        console.log("Score: " + score);
 
-let playerSelection =  prompt("enter rock, paper or scissors:");
+        // computer turn 
+        let computerSelection = computerChoice(arr);
+        // cheats!
+        // console.log("computer: " + computerSelection);
 
-// validate player selection
-if (playerSelection != "rock" || playerSelection != "paper" | playerSelection != "scissors") {
-    console.log("invalid input!")
-    playerSelection =  prompt("enter rock, paper or scissors:");
+        // player turn
+        let playerSelection =  prompt("enter rock, paper or scissors:").toLowerCase();
+
+        // check if input is valid before proceeding
+
+        if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === "scissors") {
+            console.log("player: " + playerSelection);
+            console.log(round(computerSelection, playerSelection));
+            console.log(score);
+        } else {
+            console.log(playerSelection);
+            console.log("invalid input");
+        }
+    }
+
+// game over
+if (score == 5) {
+    console.log("Congratulations, you win the game!");
 }
-console.log("player: " + playerSelection);
-console.log(round(computerSelection, playerSelection));
-
-
-
